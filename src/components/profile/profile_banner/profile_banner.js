6a4017fc486e8b35934abe664d376a30358e415d
RomanBenko <skayone1@gmail.com>
2020-09-27 22:13:25 +0300

    components refactoring

--- src/components/profile/profile_banner/profile_banner.js
+++ src/components/profile/profile_banner/profile_banner.js
@@ -1,14 +1,16 @@
 import React from "react";
 import './profile_banner.css';
 
-const ProfileBanner = () => {
+const Profile = () => {
     return (
         <div className="profileBanner card">
             <div className="profileBanner__background">
                 <img src="https://demo.hasthemes.com/adda-preview/adda/assets/images/banner/banner-small.jpg" alt=""/>
                 <div className="profileBanner__avatar">
-                    <a href="#"><img src="https://demo.hasthemes.com/adda-preview/adda/assets/images/profile/profile-midle-1.jpg" alt="" className="profileBanner__image"/></a>
-                    </div>
+                    <a href="#"><img
+                        src="https://demo.hasthemes.com/adda-preview/adda/assets/images/profile/profile-midle-1.jpg"
+                        alt="" className="profileBanner__image"/></a>
+                </div>
             </div>
             <div className="profileBanner__info">
                 <div className="profileBanner__info-title">
@@ -22,5 +24,4 @@ const ProfileBanner = () => {
     )
 }
 
-
-export default ProfileBanner;
+export default Profile;
